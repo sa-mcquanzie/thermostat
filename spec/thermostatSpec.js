d23e54a07134214d4c61b9071aba5c7e5ecfce98
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-02 18:01:03 +0100

    Adding final Thursday Commit

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -56,6 +56,18 @@ describe('Thermostat', () => {
       expect(thermostat.getCurrentTemperature()).toEqual(25);
     });
   });
+
+  describe('when Power Saving Mode is off', () => {
+    it('has a maximum temperature of 32', () => {
+      thermostat.switchPowerSavingModeOff();
+
+      for (let i = 0; i < 13; i++) {
+        thermostat.up();
+      }
+
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
 });
 
 
